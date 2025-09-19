8468e4697239d2337aeb86092c57aa4cf50cb0ea
Kevin Alvarenga <kevin.alvarenga93@gmail.com>
2025-09-19 09:07:16 -0600

    se crea nueva estructura de carpetas y archivos

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,25 +1,34 @@
 ﻿using Microsoft.Extensions.Logging;
+using Listify.Servicios;
+using Listify.ViewModels;
 
-namespace Listify
+namespace Listify;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        builder.Services.AddSingleton<BaseDeDatosServicio>();
+        builder.Services.AddSingleton<PrincipalVistaModelo>();
+        builder.Services.AddTransient<EditarArticuloVistaModelo>();
+
+        var app = builder.Build();
+
+        ServicioLocalizador.Servicios = app.Services;
+
+        return app;
     }
 }
